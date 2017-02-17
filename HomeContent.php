1ba3204cce8625bf1592541cd93cc3bac85d2e47
ngattusohw <ngattusohw@gmail.com>
2017-02-17 10:54:47 -0500

    Changing the carousel button colors, making them visiable

--- HomeContent.php
+++ HomeContent.php
@@ -4,7 +4,7 @@
 				Collect is a software company driven by ideas. Our passionate and intelligent team distribute their ideas through the company's platform and resources. Our  goal is to create innovative software to better society. The team is made up of individuals from Columbia University, Stevens Institute of Technology, Ocean County College, and Harvey Mudd College.  Our current mission is a rework of the initiative "PALS", with the objective of increasing the target audience to every special needs program in America. To read more, go to the "PALS" section.
 		</p>
 
-		<div class="container">
+		
 
 			
 			<br>
@@ -21,7 +21,7 @@
 			    <div class="carousel-inner" role="listbox">
 			      <div class="item active">
 			      	<a href="http://www.collectsoftware.org/PALS/">
-			        <img src="Content/images/pals.jpg" alt="Pals" width="30%" height="20%">
+			        <img class ="d-block img-fluid" src="Content/images/pals.jpg" alt="Pals" width="30%" height="20%">
 			        <div class="carousel-caption">
 				        <h3 class = "blackText">Panthers Assisted Learning Software</h3>
 				        <p class = "blackText">Sign up for our fun, educational games!</p>
@@ -32,7 +32,7 @@
 
 			      <div class="item">
 			        <a href="http://classroomcloseup.org/upcoming-this-season/">
-			        <img src="Content/images/classroomCloseup.jpg" alt="Classroom Closeup" width="460" height="345">
+			        <img class="d-block img-fluid" src="Content/images/classroomCloseup.jpg" alt="Classroom Closeup" width="460" height="345">
 			        <div class="carousel-caption">
 				        <h3 class = "blackText">This February 19th!</h3>
 				        <p class = "blackText">See us featured in this month's edition of Classroom Closeup</p>
@@ -42,7 +42,7 @@
 			    
 			      <div class="item">
 			      	<a href="https://www.linkedin.com/company/11022163?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A11022163%2Cidx%3A2-1-2%2CtarId%3A1486525399862%2Ctas%3Acollect%20software">
-			        <img src="Content/images/linked.jpg" alt="linkedin" width="460" height="345">
+			        <img class="d-block img-fluid" src="Content/images/linked.jpg" alt="linkedin" width="460" height="345">
 			        <div class="carousel-caption">
 				        <h3 class = "blackText">Visit Our Linked In</h3>
 				        <p class = "blackText">Connect with us!</p>
@@ -52,7 +52,7 @@
 
 			      <div class="item">
 			      	<a href="http://www.app.com/story/news/education/in-our-schools/2016/02/18/point-pleasant-students-program-better-future/80234952/">
-			        <img src="Content/images/app.jpg" alt="Chania" width="460" height="345">
+			        <img class="d-block img-fluid" src="Content/images/app.jpg" alt="Chania" width="460" height="345">
 			        <div class="carousel-caption">
 				        <h3 class = "blackText">See us featured in the news!</h3>
 				        <p class = "blackText">Our start at Point Boro</p>
@@ -75,7 +75,7 @@
 		</div>
  
 	</div>
-</div>
+
 
 
 
