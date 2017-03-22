1b0c21a8661c2b0e363bacdf5817e2bce4d3c053
ngattusohw <ngattusohw@gmail.com>
2017-03-22 19:37:34 -0400

    Removing echos to remove clutter on success page

--- FormSuccessContent.php
+++ FormSuccessContent.php
@@ -1,7 +1,7 @@
 	<div id="content" style="height: 50%;">
 		<?php 
 
-			print phpinfo();
+			
 			error_reporting(-1);
 			ini_set('display_errors', 'On');
 			set_error_handler("var_dump");
@@ -16,20 +16,17 @@
 		    $message = $_POST['message'];
 		    // In case any of our lines are larger than 70 characters, we should use wordwrap()
 		    $message = wordwrap($message, 70, "\r\n");
-		    echo "$message\n";
 		    $from = 'From: $email'; 
 		    $to = 'ngattusohw@gmail.com'; 
 		    $subject = 'Customer Inquiry';
 		    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-		    echo "Here";
 		    $headers = array("$from",
 			    "Reply-To: ngattusohw@gmail.com",
 			    "X-Mailer: PHP/" . PHP_VERSION s
 			);
 			$headers = implode("\r\n", $headers);
 			
-			echo "$headers\n";
-			echo "$body";
+			
 
 			if (mail ($to, $subject, $body, $from, $headers)) { 
 		        echo '<p>Your message has been sent!</p>';
