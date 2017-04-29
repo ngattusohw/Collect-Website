1da94d2a2abf9a4e8242628b3d4529e5afbbb8e3
ngattusohw <ngattusohw@gmail.com>
2017-04-28 21:39:01 -0400

    Finally fixing Email Integration lol

--- FormSuccessContent.php
+++ FormSuccessContent.php
@@ -1,35 +1,37 @@
 	<div id="content" style="height: 50%;">
 		<?php 
 
-			
-			error_reporting(-1);
-			ini_set('display_errors', 'On');
-			set_error_handler("var_dump");
 
+		error_reporting(-1);
+		ini_set('display_errors', 'On');
+		set_error_handler("var_dump");
 
-			
 
 			//mail('ngattusohw@gmail.com', 'My Subject', $message, $headers);
 
-			$name = $_POST['emailerName'];
+						$name = $_POST['emailerName'];
 		    $email = $_POST['email'];
 		    $message = $_POST['message'];
 		    // In case any of our lines are larger than 70 characters, we should use wordwrap()
 		    $message = wordwrap($message, 70, "\r\n");
-		    $from = 'From: $email'; 
+		    $from = 'From: ' . $email; 
 		    $to = 'ngattusohw@gmail.com'; 
 		    $subject = 'Customer Inquiry';
 		    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-		    $headers = array("$from",
+		    $headers = array($from,
 			    "Reply-To: ngattusohw@gmail.com",
-			    "X-Mailer: PHP/" . PHP_VERSION s
-			);
+			    "X-Mailer: PHP/" . PHP_VERSION);
 			$headers = implode("\r\n", $headers);
-			
-			
 
-			if (mail ($to, $subject, $body, $from, $headers)) { 
-		        echo '<p>Your message has been sent!</p>';
+			// $headers = array("From: from@example.com",
+			//     "Reply-To: replyto@example.com",
+			//     "X-Mailer: PHP/" . PHP_VERSION
+			// );
+			// $headers = implode("\r\n", $headers);
+			// mail($to, $subject, $message, $headers);
+			
+			if (mail ($to, $subject, $body, $headers)) { 
+		        echo '<p>Your message has been sent! Thank you!</p>';
 		    } else { 
 		        echo '<p>Something went wrong, go back and try again!</p>'; 
 		    }
